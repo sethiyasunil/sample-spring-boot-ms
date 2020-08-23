b8a62eb629e39fa5f071224faa58629fc2de7b2e
sunil <sethiya_sunil@yahoo.com>
2020-08-23 15:42:35 +0530

    i

---
+++ src/main/java/com/example/samplespringms/SampleSpringMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplespringms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/samplespringms/SampleSpringMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samplespringms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
